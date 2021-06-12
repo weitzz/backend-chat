bbd3b9f21eda345faf564b0dc3d33326c60ab021
Tatiane Weitzel <weitzeltati@gmail.com>
2021-06-12 20:33:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+uconst http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
@@ -23,7 +23,7 @@ io.on('connect', (socket) => {
     socket.join(user.room);
 
     socket.emit('message', { user: 'admin', text: `${user.name}, Bem vindx a sala ${user.room}.` });
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} entrou na sala!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
